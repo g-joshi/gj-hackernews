d4211678527bc1e2e1d6fce8978f195e53bddc6a
Gaurav Joshi <gauravjoshi.7286@gmail.com>
2020-06-20 17:20:15 +0530

    fixed#

--- server/server.js
+++ server/server.js
@@ -1,20 +1,20 @@
-import React from "react";
-import express from "express";
-import path from "path";
-import { renderToString } from "react-dom/server";
+import React from 'react';
+import express from 'express';
+import path from 'path';
+import { renderToString } from 'react-dom/server';
 import { renderRoutes } from 'react-router-config';
 import { StaticRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-import createSageMiddleware from "redux-saga";
+import createSageMiddleware from 'redux-saga';
 import fs from 'fs';
-import { ServerStyleSheet, StyleSheetManager } from 'styled-components'
+import { ServerStyleSheet, StyleSheetManager } from 'styled-components';
 import newsReducer from '../src/store/reducers/newsReducer';
 import { fetchNewsSaga } from '../src/store/sagas/newsSaga';
 import Routes from '../src/routes/routes.config';
 
 const sheet = new ServerStyleSheet();
-const compression = require('compression')
+const compression = require('compression');
 
 const app = express();
 const sagaMiddleware = createSageMiddleware();
@@ -24,36 +24,42 @@ app.use(compression());
 app.use(express.static(path.resolve(__dirname, '../', 'build')));
 
 app.get('/:pageId', (req, res) => {
-    sagaMiddleware.run(function* () {
-        yield fetchNewsSaga({ payload: { pageId: req.params.pageId } })
-    }).toPromise().then(_ => {
-        const content = renderToString(
-          <Provider store={store}>
-            <StaticRouter location={req.path} context={{}}>
-              <StyleSheetManager sheet={sheet.instance}>
-                {renderRoutes(Routes)}
-              </StyleSheetManager>
-            </StaticRouter>
-          </Provider>
+  sagaMiddleware
+    .run(function* anon() {
+      yield fetchNewsSaga({ payload: { pageId: req.params.pageId } });
+    })
+    .toPromise()
+    .then(() => {
+      const content = renderToString(
+        <Provider store={store}>
+          <StaticRouter location={req.path} context={{}}>
+            <StyleSheetManager sheet={sheet.instance}>
+              {renderRoutes(Routes)}
+            </StyleSheetManager>
+          </StaticRouter>
+        </Provider>
+      );
+
+      const styleTags = sheet.getStyleTags();
+
+      fs.readFile(path.resolve('build/index.html'), 'utf-8', (error, data) => {
+        if (error) {
+          res.send('something went wrong');
+        }
+        let data2 = '';
+        data2 = data.replace(
+          '<div id="root"></div>',
+          `${styleTags}<div id="root">${content}</div><script type="text/javascript" charset="utf-8">window.__INITIAL_STATE__ = ${JSON.stringify(
+            store.getState()
+          )};</script>`
         );
-
-        const styleTags = sheet.getStyleTags();
-
-        fs.readFile(path.resolve('build/index.html'), 'utf-8', (error, data) => {
-            if (error) { res.send('something went wrong'); }
-
-            data = data.replace(
-                '<div id="root"></div>',
-                `${styleTags}<div id="root">${content}</div><script type="text/javascript" charset="utf-8">window.__INITIAL_STATE__ = ${JSON.stringify(store.getState())};</script>`
-            );
-            res.send(data);
-        });
-    }, (error) => {
-        console.log(error);
+        res.send(data2);
+      });
     });
 });
 
 const port = process.env.PORT || 4000;
 app.listen(port, () => {
-    console.log('Application is up');
+  // eslint-disable-next-line
+  console.log('Application is up');
 });
